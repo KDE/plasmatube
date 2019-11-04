17ba799183a49bd8971d774729254b5a08d39c90
Linus Jahn <lnj@kaidan.im>
2019-11-04 20:59:57 +0100

    Finish YTMusic Thumbnail json

--- src/ytmusic/YTMThumbnail.cpp
+++ src/ytmusic/YTMThumbnail.cpp
@@ -1,1 +1,68 @@
+/*
+ * Copyright 2019  Linus Jahn <lnj@kaidan.im>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "YTMThumbnail.h"
+
+#include <QJsonObject>
+
+YTMThumbnail YTMThumbnail::fromJson(const QJsonObject &json)
+{
+    YTMThumbnail thumb;
+    thumb.setUrl({json.value(QStringLiteral("url")).toString()});
+    thumb.size().setWidth(json.value(QStringLiteral("width")).toInt());
+    thumb.size().setHeight(json.value(QStringLiteral("height")).toInt());
+
+    return thumb;
+}
+
+QJsonObject YTMThumbnail::toJson() const
+{
+    QJsonObject json;
+    json.insert(QStringLiteral("url"), m_url.toString(QUrl::FullyEncoded));
+    json.insert(QStringLiteral("width"), m_size.width());
+    json.insert(QStringLiteral("height"), m_size.height());
+
+    return json;
+}
+
+QSize YTMThumbnail::size() const
+{
+    return m_size;
+}
+
+QSize &YTMThumbnail::size()
+{
+    return m_size;
+}
+
+void YTMThumbnail::setSize(const QSize &size)
+{
+    m_size = size;
+}
+
+QUrl YTMThumbnail::url() const
+{
+    return m_url;
+}
+
+void YTMThumbnail::setUrl(const QUrl &url)
+{
+    m_url = url;
+}
--- src/ytmusic/YTMThumbnail.h
+++ src/ytmusic/YTMThumbnail.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019  Linus Jahn <lnj@kaidan.im>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef YTMTHUMBNAIL_H
 #define YTMTHUMBNAIL_H
 
@@ -9,7 +29,8 @@ class QJsonObject;
 class YTMThumbnail
 {
 public:
-    static YTMThumbnail &fromJson(const QJsonObject &json);
+    static YTMThumbnail fromJson(const QJsonObject &json);
+    QJsonObject toJson() const;
 
     QSize size() const;
     QSize &size();
@@ -17,8 +38,6 @@ public:
 
     QUrl url() const;
     void setUrl(const QUrl &url);
-
-    QJsonObject toJson() const;
 
 private:
     QSize m_size;
