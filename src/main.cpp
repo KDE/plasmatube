393338b120440ae7728eb720cf378cb71ba65f56
Joshua Goins <josh@redstrate.com>
2023-12-06 13:09:12 -0500

    Use qqc2-breeze-style on Android

--- src/main.cpp
+++ src/main.cpp
@@ -46,6 +46,7 @@ int main(int argc, char **argv)
 {
 #ifdef Q_OS_ANDROID
     QGuiApplication app(argc, argv);
+    QQuickStyle::setStyle(QStringLiteral("org.kde.breeze"));
 #else
     QApplication app(argc, argv);
 
